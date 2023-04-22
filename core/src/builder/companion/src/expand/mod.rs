d30fa42521ab28e7ec403113eb275f0d53e9a4cb
cdamian <17934949+cdamian@users.noreply.github.com>
2023-04-22 04:46:31 +0200

    companion: Handle builder errors

--- core/src/builder/companion/src/expand/mod.rs
+++ core/src/builder/companion/src/expand/mod.rs
@@ -23,6 +23,7 @@ pub fn expand(def: CompanionDef) -> SynResult<TokenStream> {
 	let tracing_crate = get_crate_with_name("sp-tracing")?;
 	let codec_crate = get_crate_with_name("parity-scale-codec")?;
 	let sp_std_crate = get_crate_with_name("sp-std")?;
+	let thiserror_crate = get_crate_with_name("thiserror")?;
 
 	let vis = def.vis.to_token_stream();
 	let name = def.companion_name.to_token_stream();
@@ -58,10 +59,34 @@ pub fn expand(def: CompanionDef) -> SynResult<TokenStream> {
 
 	let ts = quote! {
 		use #fudge_core_crate::builder::parachain::FudgeCollator;
+		use #fudge_core_crate::builder::parachain::Error as ParachainError;
+		use #fudge_core_crate::builder::relay_chain::Error as RelayChainError;
+		use #fudge_core_crate::builder::stand_alone::Error as StandaloneError;
 		use #fudge_crate::primitives::{Chain as _hidden_Chain, ParaId as _hidden_ParaId, FudgeParaChain as _hidden_FudgeParaChain};
 		use #codec_crate::Decode as __hidden_Decode;
 		use #tracing_crate as __hidden_tracing;
 		use #sp_std_crate::sync::{Arc, Mutex};
+		use #thiserror_crate::Error;
+
+		pub type InnerError = Box<dyn std::error::Error>;
+
+		#[derive(Error, Debug)]
+		pub enum BuilderError {
+			#[error("standalone builder: {0}")]
+			Standalone(InnerError),
+
+			#[error("parachain builder: {0}")]
+			Parachain(InnerError),
+
+			#[error("relaychain builder: {0}")]
+			Relaychain(InnerError),
+
+			#[error("extrinsic decoding: {0}")]
+			ExtrinsicDecode(InnerError),
+
+			#[error("parachain with ID '{0}' not found")]
+			ParachainNotFound(u32),
+		}
 
 		#vis struct #name {
 			#relay_vis #relay_chain_name: #relay_chain,
@@ -76,7 +101,7 @@ pub fn expand(def: CompanionDef) -> SynResult<TokenStream> {
 		}
 
 		impl #name {
-			pub fn new(#relay_chain_name: #relay_chain, #(#parachain_names: #parachain_types,)* #(#others_names: #others_types,)*) -> Result<Self, ()> {
+			pub fn new(#relay_chain_name: #relay_chain, #(#parachain_names: #parachain_types,)* #(#others_names: #others_types,)*) -> Result<Self, BuilderError> {
 				let mut companion = Self {
 					#relay_chain_name,
 					#(#parachain_names,)*
@@ -86,20 +111,20 @@ pub fn expand(def: CompanionDef) -> SynResult<TokenStream> {
 				#(
 					let para = _hidden_FudgeParaChain {
 						id: _hidden_ParaId::from(#parachain_ids),
-						head: companion.#parachain_names.head().map_err(|_| ())?,
-						code: companion.#parachain_names.code().map_err(|_| ())?,
+						head: companion.#parachain_names.head().map_err(|e| BuilderError::Parachain(e.into()))?,
+						code: companion.#parachain_names.code().map_err(|e| BuilderError::Parachain(e.into()))?,
 					};
 
 					let collator = companion.#parachain_names.collator();
 
-					companion.#relay_chain_name.onboard_para(para, Box::new(collator)).map_err(|_| ()).map(|_| ())?;
+					companion.#relay_chain_name.onboard_para(para, Box::new(collator)).map_err(|e| BuilderError::Relaychain(e.into())).map(|_| ())?;
 
 				)*
 
 				Ok(companion)
 			}
 
-			pub fn append_extrinsics(&mut self, chain: _hidden_Chain, xts: Vec<Vec<u8>>) -> Result<(), ()> {
+			pub fn append_extrinsics(&mut self, chain: _hidden_Chain, xts: Vec<Vec<u8>>) -> Result<(), BuilderError> {
 				for xt in xts {
 					self.append_extrinsic(chain, xt)?;
 				}
@@ -107,71 +132,71 @@ pub fn expand(def: CompanionDef) -> SynResult<TokenStream> {
 				Ok(())
 			}
 
-			pub fn append_extrinsic(&mut self, chain: _hidden_Chain, xt: Vec<u8>) -> Result<(), ()> {
+			pub fn append_extrinsic(&mut self, chain: _hidden_Chain, xt: Vec<u8>) -> Result<(), BuilderError> {
 				match chain {
 					_hidden_Chain::Relay => {
 						self.#relay_chain_name.append_extrinsic(
-							__hidden_Decode::decode(&mut xt.as_slice()).map_err(|_|())?
-						).map_err(|_|())?;
+							__hidden_Decode::decode(&mut xt.as_slice()).map_err(|e| BuilderError::ExtrinsicDecode(e.into()))?
+						).map_err(|e| BuilderError::Relaychain(e.into()))?;
 						Ok(())
 					},
 					_hidden_Chain::Para(id) => match id {
 						#(
 							_ if id == #parachain_ids => {
 								self.#parachain_names.append_extrinsic(
-									__hidden_Decode::decode(&mut xt.as_slice()).map_err(|_|())?
-								).map_err(|_|())?;
+									__hidden_Decode::decode(&mut xt.as_slice()).map_err(|e| BuilderError::ExtrinsicDecode(e.into()))?
+								).map_err(|e| BuilderError::Parachain(e.into()))?;
 								Ok(())
 							},
 						)*
-						_ => return Err(()),
+						_ => return Err(BuilderError::ParachainNotFound(id)),
 					}
 				}
 			}
 
-			pub fn with_state<R>(&self, chain: _hidden_Chain, exec: impl FnOnce() -> R) -> Result<R, ()> {
+			pub fn with_state<R>(&self, chain: _hidden_Chain, exec: impl FnOnce() -> R) -> Result<R, BuilderError> {
 				match chain {
-					_hidden_Chain::Relay => self.#relay_chain_name.with_state(exec).map_err(|_| ()),
+					_hidden_Chain::Relay => self.#relay_chain_name.with_state(exec).map_err(|e| BuilderError::Relaychain(e.into())),
 					_hidden_Chain::Para(id) => match id {
 						#(
-							_ if id == #parachain_ids => self.#parachain_names.with_state(exec).map_err(|_| ()),
+							_ if id == #parachain_ids => self.#parachain_names.with_state(exec).map_err(|e| BuilderError::Parachain(e.into())),
 						)*
-						_ => Err(())
+						_ => Err(BuilderError::ParachainNotFound(id))
 					}
 				}
 			}
 
-			pub fn with_mut_state<R>(&mut self,  chain: _hidden_Chain, exec: impl FnOnce() -> R) -> Result<R, ()> {
+			pub fn with_mut_state<R>(&mut self,  chain: _hidden_Chain, exec: impl FnOnce() -> R) -> Result<R, BuilderError> {
 				match chain {
-					_hidden_Chain::Relay => self.#relay_chain_name.with_mut_state(exec).map_err(|_| ()),
+					_hidden_Chain::Relay => self.#relay_chain_name.with_mut_state(exec).map_err(|e| BuilderError::Relaychain(e.into())),
 					_hidden_Chain::Para(id) => match id {
 						#(
-							_ if id == #parachain_ids => self.#parachain_names.with_mut_state(exec).map_err(|_| ()),
+							_ if id == #parachain_ids => self.#parachain_names.with_mut_state(exec).map_err(|e| BuilderError::Parachain(e.into())),
 						)*
-						_ => Err(())
+						_ => Err(BuilderError::ParachainNotFound(id))
 					}
 				}
 			}
 
-			pub fn evolve(&mut self) -> Result<(), ()> {
+			pub fn evolve(&mut self) -> Result<(), BuilderError> {
 				{
 					__hidden_tracing::enter_span!(sp_tracing::Level::TRACE, std::stringify!(#relay_chain_name - BlockBuilding:));
-					self.#relay_chain_name.build_block().map_err(|_| ()).map(|_| ())?;
-					self.#relay_chain_name.import_block().map_err(|_| ()).map(|_| ())?;
+					self.#relay_chain_name.build_block().map_err(|e| BuilderError::Relaychain(e.into())).map(|_| ())?;
+					self.#relay_chain_name.import_block().map_err(|e| BuilderError::Relaychain(e.into())).map(|_| ())?;
 				}
 
 				{
 					#(
 						__hidden_tracing::enter_span!(sp_tracing::Level::TRACE, std::stringify!(#parachain_names - BlockBuilding:));
-						self.#parachain_names.build_block().map_err(|_| ()).map(|_| ())?;
-						self.#parachain_names.import_block().map_err(|_| ()).map(|_| ())?;
+						self.#parachain_names.build_block().map_err(|e| BuilderError::Parachain(e.into())).map(|_| ())?;
+						self.#parachain_names.import_block().map_err(|e| BuilderError::Parachain(e.into())).map(|_| ())?;
 					)*
 				}
 
 				{
 					__hidden_tracing::enter_span!(sp_tracing::Level::TRACE, std::stringify!(#relay_chain_name - BlockBuilding:));
-					self.#relay_chain_name.build_block().map_err(|_| ()).map(|_| ())?;
-					self.#relay_chain_name.import_block().map_err(|_| ()).map(|_| ())?;
+					self.#relay_chain_name.build_block().map_err(|e| BuilderError::Relaychain(e.into())).map(|_| ())?;
+					self.#relay_chain_name.import_block().map_err(|e| BuilderError::Relaychain(e.into())).map(|_| ())?;
 				}
 
 				{
@@ -179,13 +204,13 @@ pub fn expand(def: CompanionDef) -> SynResult<TokenStream> {
 						__hidden_tracing::enter_span!(sp_tracing::Level::TRACE, std::stringify!(#relay_chain_name - Onboarding(#parachain_names):));
 						let para = _hidden_FudgeParaChain {
 							id: _hidden_ParaId::from(#parachain_ids),
-							head: self.#parachain_names.head().map_err(|_| ())?,
-							code: self.#parachain_names.code().map_err(|_| ())?,
+							head: self.#parachain_names.head().map_err(|e| BuilderError::Parachain(e.into()))?,
+							code: self.#parachain_names.code().map_err(|e| BuilderError::Parachain(e.into()))?,
 						};
 
 						let collator = self.#parachain_names.collator();
 
-						self.#relay_chain_name.onboard_para(para, Box::new(collator)).map_err(|_| ()).map(|_| ())?;
+						self.#relay_chain_name.onboard_para(para, Box::new(collator)).map_err(|e| BuilderError::Relaychain(e.into())).map(|_| ())?;
 					)*
 				}
 
